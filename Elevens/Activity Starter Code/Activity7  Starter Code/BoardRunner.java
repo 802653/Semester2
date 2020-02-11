9c46acf3cf79c07f0649987c653ef71e327726a2
802653 <54371363+802653@users.noreply.github.com>
2020-02-10 19:49:50 -0800

    sgasag
    asg

---
+++ Elevens/Activity Starter Code/Activity7  Starter Code/BoardRunner.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class BoardRunner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class BoardRunner
+{
+    public static void main(){
+        ElevensBoard board = new ElevensBoard();
+        board.printCards(board);
+    
+    
+    }
+    
+    
+}
